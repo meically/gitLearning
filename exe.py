cd71dc652e876cdb0a9dd50fda7343b9ff005330
贝母 <506599539@qq.com>
2018-08-14 00:07:21 +0800

    20180804练习
    字符串，列表练习

---
+++ exe.py
@@ -0,0 +1,18 @@
+
+a=[1,2,3,4,5,6,7,8,9]
+b=list(reversed(a))
+print(b)
+
+
+'''
+a=[1,2,3,4,5,6,7,8,9]
+c=sorted(a, key=None, reverse=True)
+print(c)
+
+
+
+# 方式三: 使用分片
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+d = a[::-1]
+print(d)
+'''
